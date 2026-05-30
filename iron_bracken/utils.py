594e8fe1ee8c232b6c09ae49f548a761c8aceea9
Icii White <mail.iciiwhite@gmail.com>
2026-05-30 15:20:48 +0300

    Create new file

---
+++ iron_bracken/utils.py
@@ -0,0 +1,15 @@
+import uuid
+import socket
+
+def generate_node_id():
+    return str(uuid.uuid4()).replace('-', '')[:16]
+
+def get_local_ip():
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.connect(('8.8.8.8', 80))
+        ip = s.getsockname()[0]
+        s.close()
+        return ip
+    except Exception:
+        return '127.0.0.1'
